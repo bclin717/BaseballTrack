f6bfed762622dd24b6c211ffea51af044844391e
Kevin <ds934118@gmail.com>
2017-07-05 14:27:15 +0800

    V1 tracker

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 using namespace cv;
 
-char* urlOfVideo1 = "C:\\Users\\Kevin\\Desktop\\Baseball Project\\video\\146km.mp4";
+//char* urlOfVideo1 = "C:\\Users\\Kevin\\Desktop\\Baseball Project\\video\\146km.mp4";
+char* urlOfVideo1 = "C:\\Users\\ds934\\Desktop\\video\\change.mp4";
 Ptr<BackgroundSubtractor> pKNN;
 
 int main() {
@@ -86,8 +87,14 @@ int main() {
 			}
 		}
 
+		
 		centerPointOfBallsInAllPic.push_back(Point(-1,-1));
-		if (ballsBox.size() <= 0) continue;
+		if (ballsBox.size() <= 0) {
+			centerPointOfBallsInAllPic.at(i) = centerPointOfBallsInAllPic.at(i - 1);
+			centerPointOfBallsInAllPic.at(i).x += 10;
+			centerPointOfBallsInAllPic.at(i).y += 6;
+			continue;
+		}
 
 		//計算中心點
 		vector<Point> centers;
@@ -96,34 +103,91 @@ int main() {
 		for (int j = 0; j < ballsBox.size(); j++) {
 			centers.push_back(Point(ballsBox[j].x + ballsBox[j].width / 2, ballsBox[j].y + ballsBox[j].height / 2));
 
-			if (centers.at(j).y > MaxY) MaxY = centers.at(j).y;
 		}
+		
+		if (i == 0) {
+			//第一次選出最左上角的點
+			int xTarget = 99999, yTarget = 99999;
+			int xPos = -1, yPos = -1;
+			int num = 0;
+			
+			//x,y都dominate
+			for (int j = 0; j < centers.size(); j++) {
+				if (centers.at(j).x < xTarget) {
+					xTarget = centers.at(j).x;
+					xPos = j;
+				}
+				if (centers.at(j).y < yTarget) {
+					yTarget = centers.at(j).y;
+					yPos = j;
+				}
+			}
 
-		//選出最左上角的點
-		int target = 99999;
-		int num = 0;
-		for (int j = 0; j < centers.size(); j++) {
-			if (centers.at(j).x + MaxY - centers.at(j).y < target) {
-				target = centers.at(j).x + MaxY - centers.at(j).y;
-				num = j;
+			if (xPos == yPos) {
+				centerPointOfBallsInAllPic.at(i) = Point(centers.at(xPos).x, centers.at(xPos).y);
+			} else {
+				//無法Domiate
+				int Target = 99999;
+				int position = -1;
+				int MaxY = 155;
+				for (int j = 0; j < centers.size(); j++) {
+					if (centers.at(j).x + MaxY - centers.at(j).y < Target) {
+						Target = centers.at(j).x + MaxY - centers.at(j).y;
+						position = j;
+					}
+				}
+				centerPointOfBallsInAllPic.at(i) = Point(centers.at(position).x, centers.at(position).y);
+			}
+
+
+			
+		} else {
+			//選出可能的點
+			const int xMoves = 50;
+			const int yMoves = 30;
+			Point preprePoint;
+			if(i>=2)
+				preprePoint = centerPointOfBallsInAllPic.at(i - 2);
+			Point previousPoint = centerPointOfBallsInAllPic.at(i - 1);
+			Point currentPoint;
+			for (int j = 0; j < centers.size(); j++) {
+				currentPoint = centers.at(j);
+
+				if (previousPoint.x <= currentPoint.x &&
+					currentPoint.x <= previousPoint.x + xMoves &&
+					previousPoint.y - 10 <= currentPoint.y &&
+					currentPoint.y <= previousPoint.y + yMoves) {
+					
+					centerPointOfBallsInAllPic.at(i) = Point(currentPoint.x, currentPoint.y);
+					break;
+				}
+			}
+
+			if (centerPointOfBallsInAllPic.at(i).x == -1 &&
+				centerPointOfBallsInAllPic.at(i).y == -1) {
+
+				centerPointOfBallsInAllPic.at(i).x = 4 + previousPoint.x;
+				centerPointOfBallsInAllPic.at(i).y = 4 + previousPoint.y;
 			}
 		}
-
-		centerPointOfBallsInAllPic.at(i) = Point(centers.at(num).x, centers.at(num).y);
-
-
+		
+		
 	}
 	
 	for (int i = 0; i < centerPointOfBallsInAllPic.size(); i++) {
 		cout << "No. " << i << " X : " << centerPointOfBallsInAllPic.at(i).x << " Y : " << centerPointOfBallsInAllPic.at(i).y << endl;
 	}
 
-
+	for (int i = 0; i < framesROI.size()-1; i++) {
+		circle(framesROI.at(i+1), centerPointOfBallsInAllPic.at(i), 4, Scalar(0, 0, 255), -1);
+	}
+	
+	
 	
 
 	int i = 0;
 	while(waitKey(10) != 27 || i < frontObject.size()) {
-		//imshow("Source", framesROI.at(i));
+		imshow("Source", framesROI.at(i+2));
 		//imshow("front", frontObject.at(i));
 		imshow("White", whiteFrontObjectMoves.at(i));
 		
@@ -132,6 +196,3 @@ int main() {
 
 	return 0;
 }
-
-
-
